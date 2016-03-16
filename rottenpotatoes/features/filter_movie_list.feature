1831386c6a78e766f4f8361949ab2fc1c31d2312
Nick Phillips <phillipn101@gmail.com>
2016-03-16 04:46:15 +0000

    Final commit

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -22,11 +22,25 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG, R
+  And I uncheck the following ratings: G, PG-13
+  When I press "Refresh"
+  Then I should see the following movies
+  | title                   | rating | release_date |
+  | The Terminator          | R      | 26-Oct-1984  |
+  | When Harry Met Sally    | R      | 21-Jul-1989  |
+  | Amelie                  | R      | 25-Apr-2001  |
+  | The Incredibles         | PG     | 5-Nov-2004   |
+  | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
+  And I should not see the following movies
+  | title                   | rating | release_date |
+  | Aladdin                 | G      | 25-Nov-1992  |
+  | The Help                | PG-13  | 10-Aug-2011  |
+  | Chocolat                | PG-13  | 5-Jan-2001   |
+  | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
+  | Chicken Run             | G      | 21-Jun-2000  |
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: PG, R, G, PG-13
+  And I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,18 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  Movie.create!(:title => movie['title'], :rating => movie['rating'], :release_date => movie['release_date']) 
   end
-  fail "Unimplemented"
+end
+
+Then /^(?:|I )should (not )?see the following movies$/ do |boolean, movie_table|
+  movie_table.hashes.each do |movie_row|
+    if !boolean then
+      page.body.index(movie_row[:title]).should_not == nil
+    else
+      page.body.index(movie_row[:title]).should == nil
+    end
+  end
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +24,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.index(e1).should < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +35,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+
+  rating_list.gsub(/\s+/, '').split(",").each do |rating|
+    uncheck ? uncheck('ratings_'+rating) : check('ratings_'+rating)
+  end
+
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  within 'tbody' do
+  all('tr').count.should == 10
+  end
 end
